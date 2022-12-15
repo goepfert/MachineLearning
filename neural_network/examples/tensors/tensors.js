ca0147bdc7b17fe21a1cff145dda216d4ba0b9e5
Thomas Goepfert <thomas.goepfert@solderlab.de>
2022-12-15 17:19:49 +0100

    tensors

---
+++ neural_network/examples/tensors/tensors.js
@@ -0,0 +1,77 @@
+const row_1 = [1, 2, 3];
+const row_2 = [4, 5, 6];
+
+const array2d_1 = [
+  [1, 2, 3],
+  [4, 5, 6],
+];
+
+let array2d_2 = [];
+array2d_2.push(row_1);
+array2d_2.push(row_2);
+
+// Arrays should be equal
+// console.table(array2d_1);
+// console.table(array2d_2);
+
+// Access the elements from left to right
+const rowIdx = 0;
+const colIdx = 1;
+let elem = array2d_1[rowIdx][colIdx];
+// console.log(elem);
+
+// Create a rank-2 tensor (matrix) matrix tensor from a multidimensional array.
+const a = tf.tensor(array2d_1);
+// console.log('shape:', a.shape);
+// a.print();
+
+// Creating a 2d Tensor from flat array
+const b = tf.tensor([1, 2, 3, 4, 5, 6], [2, 3]);
+// console.log('shape:', b.shape);
+// b.print();
+
+// ----- more on shapes --------------------------------
+
+const data = Array.from(Array(30), () => {
+  return Math.floor(Math.random() * 100);
+});
+// console.log(data);
+
+//const c = tf.tensor(data, [2, 5, 3]);
+//more readable
+const c = tf.tensor3d(data, [2, 5, 3]);
+// console.log('shape:', c.shape);
+// c.print();
+
+//--- data --------------------------------
+
+// console.log(c.data());
+//console.log(c.dataSync());
+// c.data().then((data) => console.log(data));
+
+async function print() {
+  let d = await c.data();
+  console.log(d);
+}
+// print();
+
+//  ---- memory --------------------------------
+
+function compute() {
+  const d1 = Array.from(Array(3000), () => {
+    return Math.floor(Math.random() * 100);
+  });
+  const t1 = tf.tensor2d(d1, [500, 6]);
+  const t2 = tf.tensor2d(d1, [500, 6]);
+
+  const res = t1.add(t2);
+
+  //   res.print();
+  console.log(tf.memory().numTensors);
+}
+
+compute();
+
+// setInterval(() => {
+//   tf.tidy(compute);
+// }, 100);
