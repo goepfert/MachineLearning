35900ebc2b14ba6747f6a57714e1b3453c107690
Thomas Goepfert <thomas.goepfert@solderlab.de>
2022-12-18 22:35:50 +0100

    tf js

--- neural_network/examples/doodle_cnn/Dataset.js
+++ neural_network/examples/doodle_cnn/Dataset.js
@@ -3,11 +3,14 @@
  */
 
 import Utils from '../../../Utils.js';
+import { Labels } from './Labels.js';
 
 ('use strict');
 
-function createImageDataset() {
+function createImageDataset(img_width, img_height) {
   let _data = [];
+  const _img_width = img_width;
+  const _img_height = img_height;
 
   // add data with label to the record
   function addData(data, label) {
@@ -50,12 +53,47 @@ function createImageDataset() {
     }
   }
 
+  function getTrainingData() {
+    const nDatasets = _data.length;
+
+    return nextTestBatch(nDatasets, false);
+  }
+
+  function nextTestBatch(size, randomize = true) {
+    const labelList = Object.keys(Labels);
+    let xData = [];
+    let yData = [];
+
+    let yaIdx;
+    for (let idx = 0; idx < size; idx++) {
+      if (randomize) {
+        yaIdx = Math.floor(Math.random() * _data.length);
+      } else {
+        yaIdx = idx;
+      }
+      xData.push(_data[yaIdx].data);
+      yData.push(labelList.indexOf(_data[yaIdx].label));
+    }
+
+    let xs = tf.tensor(xData, [xData.length, _img_width, _img_height, 1]);
+    let labelstensor = tf.tensor1d(yData, 'int32');
+    let ys = tf.oneHot(labelstensor, labelList.length);
+    labelstensor.dispose();
+
+    return {
+      x: xs,
+      y: ys,
+    };
+  }
+
   return {
     addData,
     getData,
     clearData,
     setData,
     saveData,
+    getTrainingData,
+    nextTestBatch,
     printInfo,
   };
 }
